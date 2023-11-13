937072e875b10fe7c66470056159eb52ff8e87c6
Claire Fraser <clairefraser121@gmail.com>
2023-11-13 14:03:58 +0000

    Function timeblocks, append container

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,22 @@ document.addEventListener('DOMContentLoaded', function() {
     // Add your other JavaScript code for dynamic functionality here
     // For example, you can add code for generating timesblocks, handling user interactions, local storage, etc.
 })
+
+// Function to generate timeblocks and append them to the container
+function generateTimeblock() {
+    // Get the container element
+    const container = document.querySelector('.container');
+
+    // Standard business hours (adjust as needed)
+    const businessHours = ['9 AM', '10AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM'];
+
+    // Loop through the business hours
+    businessHours.forEach(hours=> {
+        // Create a div element for each timeblock
+        const timeblock = document.createElement('div');
+        timeblock.classList.add('row', 'time-block');
+
+        // Create an element to display the hour
+        
+    })
+}
